4e3ed687528011a542d06e180bd7cc95e74803de
Maximus626 <51446901+Maximus626@users.noreply.github.com>
2019-08-06 11:42:07 -0300

    Add files via upload

---
+++ Ejercicios_Clases_2/Ejercicio_Clases_06082019_Repaso02/Ventana.java
@@ -0,0 +1,37 @@
+package Ejercicio_Clases_06082019_Repaso02;
+
+public class Ventana {
+
+    private String marco;
+    private String vidrio;
+    private String color;
+
+    public Ventana() {
+        
+    }
+
+    public String getMarco() {
+        return marco;
+    }
+
+    public void setMarco(String marco) {
+        this.marco = marco;
+    }
+
+    public String getVidrio() {
+        return vidrio;
+    }
+
+    public void setVidrio(String vidrio) {
+        this.vidrio = vidrio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
