3a646bf1b1101ebc86dd8e2f73c4e952b4606071
Maximus626 <51446901+Maximus626@users.noreply.github.com>
2019-11-07 12:14:20 -0300

    Add files via upload

--- Ejercicios_En_Clase/BDD/BD_SISTEMAS/Jframe_Sistemas.java
+++ Ejercicios_En_Clase/BDD/BD_SISTEMAS/Jframe_Sistemas.java
@@ -3,11 +3,14 @@ package BD_SISTEMAS;
 
 import com.mysql.jdbc.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 
 public class Jframe_Sistemas extends javax.swing.JFrame {
@@ -19,7 +22,45 @@ public class Jframe_Sistemas extends javax.swing.JFrame {
         this.cn = con.coneccion();
         initComponents();
     }
-
+    
+    public void mostrar(){
+    
+    DefaultTableModel modelo = new DefaultTableModel();
+    modelo.addColumn("id");
+    modelo.addColumn("id");
+    modelo.addColumn("id");
+    modelo.addColumn("id");
+    modelo.addColumn("id");
+    Tabla.setModel(modelo);
+    
+    String sql = "Select * from usuario";
+    
+    Statement st;
+    
+    String datos [] = new String[5];
+    
+    
+        try {
+            st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()){
+            datos[0] = rs.getString(1);
+            datos[1] = rs.getString(2);
+            datos[2] = rs.getString(3);
+            datos[3] = rs.getString(4);
+            datos[4] = rs.getString(5);
+            modelo.addRow(datos);
+            
+            }
+            Tabla.setModel(modelo);
+        } catch (SQLException ex) {
+            Logger.getLogger(Jframe_Sistemas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    
+    
+    
+            }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -43,6 +84,8 @@ public class Jframe_Sistemas extends javax.swing.JFrame {
         txtCorreo = new javax.swing.JTextField();
         btnGuardar = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        Tabla = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -81,7 +124,7 @@ public class Jframe_Sistemas extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(btnGuardar))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -120,15 +163,33 @@ public class Jframe_Sistemas extends javax.swing.JFrame {
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
+        Tabla.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(Tabla);
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(36, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 233, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(248, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -171,7 +232,7 @@ public class Jframe_Sistemas extends javax.swing.JFrame {
             txtApellido.setText("");
             txtEdad.setText("");
             txtCorreo.setText("");
-            
+            mostrar();
             
         } catch (SQLException ex) {
             Logger.getLogger(Jframe_Sistemas.class.getName()).log(Level.SEVERE, null, ex);
@@ -218,6 +279,7 @@ public class Jframe_Sistemas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable Tabla;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -226,6 +288,7 @@ public class Jframe_Sistemas extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSplitPane jSplitPane1;
     private javax.swing.JTextField txtApellido;
     private javax.swing.JTextField txtCorreo;
